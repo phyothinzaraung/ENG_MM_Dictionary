d4906536351ae81c49fa87a466c22c4d7605a6e3
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2023-12-27 21:06:33 -0600

    update: dependencies update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,12 +56,14 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.21-1.0.16")
     //Room
     val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
@@ -71,7 +73,7 @@ dependencies {
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
     // Jetpack Compose Navigation
-    val nav_version = "2.7.5"
+    val nav_version = "2.7.6"
     implementation("androidx.navigation:navigation-compose:$nav_version")
     //Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.48.1")
